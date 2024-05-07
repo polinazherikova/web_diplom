80b02ae55f3daec648b5373b7f074036ba416e25
polinazherikova <157631692+polinazherikova@users.noreply.github.com>
2024-05-08 01:06:42 +0300

    Add files via upload

---
+++ diplom/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+from . import views
+from . import my_telegram_bot
+
+urlpatterns=[
+    path("", views.index, name="index"),
+    path("contact", views.contact, name="contact"),
+    path("about", views.aboutus, name="aboutus"),
+    path("faqs", views.faqs, name="faqs"),
+    path("gallery", views.gallery, name="gallery"),
+    path("portfolio", views.portfolio, name="portfolio"),
+    path("service", views.service, name="service"),
+    path('webhook/', views.webhook, name='webhook'),
+]
